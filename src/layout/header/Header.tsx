f9eaa0b09fbb22bb1a2770b2c8e67d4c30ab9db0
Ignat60 <dedaignat@gmail.com>
2023-09-11 21:47:32 +0300

    Adding  google fonts theme container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../assets/components/logo/Logo";
 import { Menu } from "../../assets/components/menu/Menu";
+import { Container } from "../../assets/components/Container";
+import { FlexWrapper } from "../../assets/components/flexWrapper";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
-      <Menu menuItems={items} />
+      <Container>
+        <FlexWrapper justify={"space-between"} align={"center"}>
+          <Logo />
+          <Menu menuItems={items} />
+        </FlexWrapper>
+      </Container>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
   background-color: #b2f2c7;
-  display: flex;
-  justify-content: space-between;
 `;
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+// import { theme } from "./Theme";
+import { theme } from "./Theme";
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        }
+
+    body {
+  margin: 0;
+  font-family:'Poppins' -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: ${theme.color.font}; 
+}
+
+a {
+    text-decoration: none;
+}
+ul {
+    list-style:none;
+}
+button {
+    background-color: unset;
+    border: unset;
+}
+
+
+Main {
+    background-color: ${theme.color.secondaryBg};
+}
+
+section:nth-of-type(odd) {
+    background-color: ${theme.color.primaryBg};
+}
+
+section:nth-of-type(even) {
+    background-color: ${theme.color.secondaryBg};
+}
+`;
