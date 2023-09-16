ca73b87e4ab93449207cf54ce9b72e0ae30e4e63
Ignat60 <dedaignat@gmail.com>
2023-09-16 12:24:10 +0300

    Testimony styled_2

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import { SectionTitle } from "../../../assets/components/SectionTitle";
 import { Icon } from "../../../assets/components/icon/Icon";
 import { Slider } from "../../../assets/components/slider/Slider";
 import { FlexWrapper } from "../../../assets/components/flexWrapper";
+import { IconWrapper } from "../skills/skill/skill";
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
       <SectionTitle>Testimony</SectionTitle>
       <FlexWrapper direction={"column"} align={"center"}>
-        <Icon iconId={"quoteAlt"} />
+        <IconWrapper>
+          <Icon iconId={"quoteAlt"} />
+        </IconWrapper>
         <Slider />
       </FlexWrapper>
     </StyledTestimony>
@@ -20,4 +23,8 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
   background-color: #e7eeff;
   min-height: 50vh;
+
+  ${IconWrapper} {
+    margin: 52px 0 72px;
+  }
 `;
