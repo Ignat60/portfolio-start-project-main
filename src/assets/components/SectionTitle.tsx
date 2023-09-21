7fb474a271910ba3c75549f41b4b61065e4e3346
Ignat60 <dedaignat@gmail.com>
2023-09-21 15:27:49 +0300

    Skills styled_3 adaptiv

--- src/assets/components/SectionTitle.tsx
+++ src/assets/components/SectionTitle.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { theme } from "../../styles/Theme";
+import { font } from "../../styles/Common";
 
 export const SectionTitle = styled.h2`
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 36px;
-  font-weight: 600;
+  ${font({
+    family: "'Josefin Sans', sans-serif",
+    weight: 600,
+    Fmax: 36,
+    Fmin: 30,
+  })}
   letter-spacing: 5px;
   text-align: center;
   margin-bottom: 90px;
@@ -22,5 +26,9 @@ export const SectionTitle = styled.h2`
     left: 50%;
     bottom: -30px;
     transform: translateX(-50%);
+
+    @media ${theme.media.mobile} {
+      bottom: -24px;
+    }
   }
 `;
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -44,6 +44,10 @@ Main {
  
 section {
     padding: 100px 0 ;
+
+    @media ${theme.media.mobile} {
+        padding: 80px 0;
+    }
 } 
 
 section:nth-of-type(odd) {
