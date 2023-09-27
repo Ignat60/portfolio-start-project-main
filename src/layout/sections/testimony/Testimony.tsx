38243f0cdb57264b851e9c8bb435cf7c3601dc35
Ignat60 <dedaignat@gmail.com>
2023-09-27 14:16:18 +0300

    Section works refactoring

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,7 +4,7 @@ import { SectionTitle } from "../../../assets/components/SectionTitle";
 import { Icon } from "../../../assets/components/icon/Icon";
 import { Slider } from "../../../assets/components/slider/Slider";
 import { FlexWrapper } from "../../../assets/components/flexWrapper";
-import { IconWrapper } from "../skills/skill/skill";
+import { S } from "./../skills/Skills_Styles";
 import { Container } from "../../../assets/components/Container";
 
 export const Testimony = () => {
@@ -13,9 +13,9 @@ export const Testimony = () => {
       <Container>
         <SectionTitle>Testimony</SectionTitle>
         <FlexWrapper direction={"column"} align={"center"}>
-          <IconWrapper>
+          <S.IconWrapper>
             <Icon iconId={"quoteAlt"} />
-          </IconWrapper>
+          </S.IconWrapper>
           <Slider />
         </FlexWrapper>
       </Container>
@@ -27,7 +27,7 @@ const StyledTestimony = styled.section`
   background-color: #e7eeff;
   min-height: 50vh;
 
-  ${IconWrapper} {
+  ${S.IconWrapper} {
     margin: 32px 0 69px;
     /* margin: 52px 0 72px; */
   }
